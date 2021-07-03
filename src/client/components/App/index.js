9250365cc74f187bcf64b768faa0c8b9cc385e14
Josh Ellington <josh.ellington@freshconsulting.com>
2021-07-03 16:13:41 -0700

    switched to airtable for station data, tweaks

--- src/client/components/App/index.js
+++ src/client/components/App/index.js
@@ -1,241 +1,99 @@
-const { ipcRenderer, shell } = require("electron");
-import React from 'react';
-import {Howl, Howler} from 'howler';
-import { xml2js, xml2json } from 'xml-js';
-import { randomProperty, stringToHTML } from '../../utils';
+const { ipcRenderer, shell } = require('electron')
+import React from 'react'
+import { Howl, Howler } from 'howler'
+import { xml2js, xml2json } from 'xml-js'
+import Airtable from 'airtable'
+import { randomProperty, stringToHTML } from '../../utils'
 
-import useInterval from './hooks/useInterval';
-import "./styles.scss"
+import useInterval from './hooks/useInterval'
+import './styles.scss'
 
 const proxy = `https://proxhole.herokuapp.com`
 
-const stations = {
-  "kmhd": {
-    name: "KMHD",
-    stream: "https://ais-sa3.cdnstream1.com/2442_128.aac?listenerId=esAdblock0306361&aw_0_1st.playerid=esPlayer&aw_0_1st.skey=1624571649",
-    homepage: "https://kmhd.org/",
-    nowPlaying: {
-      url: "https://feed.tunein.com/profiles/s34028/nowPlaying?token=eyJwIjpmYWxzZSwidCI6IjIwMjEtMDYtMjRUMjI6Mjg6NTEuMjM3MjcwMloifQ&formats=mp3,aac,ogg,flash,html,hls&serial=6fcf2c78-e114-4d6e-b1dd-e89f50abac3e&partnerId=RadioTime&version=4.63&itemUrlScheme=secure&reqAttempt=1",
-      type: "json",
-      paths: ["Header.Subtitle"],
-    },
-  },
-  "kisn": {
-    name: "KISN Good Guy Radio",
-    stream: "https://ice6.securenetsystems.net/KISNLP",
-    homepage: "https://www.goodguyradio.com/",
-    nowPlaying: {
-      url: `https://streamdb8web.securenetsystems.net/player_status_update/KISNLP.xml?randStr=${Math.random()}`,
-      type: "xml",
-      paths: ["playlist.artist._text", "playlist.title._text"],
-    },
-  },
-  "xray": {
-    name: "XRAY.FM",
-    stream: "https://listen.xray.fm/stream",
-    homepage: "https://xray.fm/",
-    nowPlaying: {
-      url: "https://xray.fm/api/broadcasting",
-      type: "json",
-      paths: ["now.title"],
-      proxy: true,
-    },
-  },
-  "kboo": {
-    name: "KBOO",
-    stream: "https://live.kboo.fm:8443/high",
-    homepage: "https://kboo.fm/",
-    nowPlaying: {
-      url: `https://kboo.fm/api/schedule/episode/one/at/${Math.floor(Date.now() / 1000)}`,
-      type: "json",
-      paths: ["0.title"],
-      proxy: true,
-    },
-  },
-  "outlaw": {
-    name: "Outlaw.fm",
-    stream: "https://usa17.fastcast4u.com/proxy/kievradio?mp=/1",
-    homepage: "https://www.outlaw.fm/",
-    nowPlaying: {
-      url: "https://fastcast4u.com/player/kievradio/index.php?c=Outlaw%20Country%20128kb%20MP3&_=1624598403038",
-      type: "json",
-      paths: ["artist", "title"],
-      proxy: true,
-    },
-  },
-  "opb": {
-    name: "OPB",
-    stream: "https://ais-sa3.cdnstream1.com/2440_128.aac?listenerId=esAdblock0306361&aw_0_1st.playerid=esPlayer&aw_0_1st.skey=1624598603",
-    homepage: "https://www.opb.org/",
-    nowPlaying: {
-      url: "https://feed.tunein.com/profiles/s34542/nowPlaying?itemToken=BgUFAAEAAQABAAEAb28B7oYAAAEFAAA&formats=mp3,aac,ogg,flash,html,hls&serial=6fcf2c78-e114-4d6e-b1dd-e89f50abac3e&partnerId=RadioTime&version=4.63&itemUrlScheme=secure&reqAttempt=1",
-      type: "json",
-      paths: ["Header.Subtitle"],
-    },
-  },
-  "kmhd2": {
-    name: "KMHD2",
-    // stream: "https://v7player.wostreaming.net/1181",
-    // stream: "https://live.wostreaming.net/playlist/mhcc-kmhchd2aac-ibc1.m3u?source=v6player",
-    stream: "https://live.wostreaming.net/direct/mhcc-kmhchd2aac-ibc1",
-    homepage: "http://kmhd2.org/",
-    nowPlaying: {
-      url: "https://scraper2.onlineradiobox.com/us.kmhdhd2?l=0",
-      type: "json",
-      paths: ["title"],
-    },
-  },
-  "chirp": {
-    name: "CHIRP",
-    stream: "https://peridot.streamguys1.com:5185/live",
-    homepage: "https://chirpradio.org/",
-    nowPlaying: {
-      url: "https://storage.googleapis.com/chirpradio-public/playlist.json",
-      type: "json",
-      paths: ["now_playing.artist", "now_playing.track"],
-      proxy: true,
-    },
-  },
-  "kanw": {
-    name: "KANW",
-    // stream: "https://14923.live.streamtheworld.com/KANWFM.mp3?dist=triton-widget&tdsdk=js-2.9&pname=tdwidgets&pversion=2.9&banners=728x90&sbmid=923c1dd3-4c20-4c86-de63-664e8ce953cc",
-    stream: "https://14923.live.streamtheworld.com/KANWFM.mp3",
-    homepage: "https://www.kanw.com/",
-    nowPlaying: {
-      url: "https://api.composer.nprstations.org/v1/widget/53a977d2e1c84b54cdcb9d51/now?format=json&_=1624602631898",
-      type: "json",
-      paths: ["onNow.program.name"],
-    },
-  },
-  "kexp": {
-    name: "KEXP",
-    stream: "https://kexp-mp3-128.streamguys1.com/kexp128.mp3",
-    homepage: "https://www.kexp.org/",
-    nowPlaying: {
-      url: "https://api.kexp.org/v2/plays/?format=json&limit=1&ordering=-airdate",
-      type: "json",
-      paths: ["results.0.artist", "results.0.song"],
-    },
-  },
-  "wnrn": {
-    name: "WNRN",
-    stream: "https://22113.live.streamtheworld.com/WNRNFM_128.mp3",
-    homepage: "https://www.wnrn.org/",
-    nowPlaying: {
-      url: "https://scraper2.onlineradiobox.com/us.w276bz",
-      type: "json",
-      paths: ["title"],
-    },
-  },
-  "kxua": {
-    name: "KXUA",
-    stream: "http://kxua.uark.edu:8830/;",
-    homepage: "http://kxua.uark.edu/",
-    nowPlaying: {
-      url: "https://feed.tunein.com/profiles/s26583/nowPlaying",
-      type: "json",
-      paths: ["Header.Subtitle"],
-      proxy: true,
-    },
-  },
-  "wxyc": {
-    name: "WXYC",
-    stream: "https://audio-mp3.ibiblio.org/wxyc.mp3",
-    homepage: "https://wxyc.org/",
-    nowPlaying: null,
-  },
-  "wyms": {
-    name: "WYMS",
-    stream: "https://wyms.streamguys1.com/live?platform=88nine",
-    homepage: "https://radiomilwaukee.org/",
-    nowPlaying: {
-      url: "https://scraper2.onlineradiobox.com/us.wyms",
-      type: "json",
-      paths: ["title"],
-    },
-  },
-  "waps": {
-    name: "The Summit",
-    stream: "https://securestreams2.autopo.st:1194/wapshq",
-    homepage: "https://thesummit.fm/",
-    nowPlaying: {
-      url: "https://thesummit.fm/audio/hd1/hd1_play.php",
-      type: "html",
-      paths: null,
-      selector: ["body"],
-      proxy: true,
-    },
-  },
-  "vmfm": {
-    name: "VMFM",
-    stream: "https://wvmw.streamguys1.com/live",
-    homepage: "http://vmfm917.org/",
-    nowPlaying: {
-      url: "https://feed.tunein.com/profiles/s23570/nowPlaying",
-      type: "json",
-      paths: ["Header.Subtitle"],
-      proxy: true,
-    },
-  },
-  "wort": {
-    name: "WORT",
-    stream: "https://wortcast01.wortfm.org:8443/high.mp3",
-    homepage: "https://www.wortfm.org/",
-    nowPlaying: {
-      url: "https://scraper2.onlineradiobox.com/us.wort?l=0",
-      type: "json",
-      paths: ["title"],
-    },
-  },
-}
+Airtable.configure({ apiKey: 'keyc7A763SxiuvCLu' })
+const airtableDb = Airtable.base('appnU2lNzTVBE6dgV')
 
 const App = () => {
+  const [stations, setStations] = React.useState({})
   const [currentStation, setCurrentStation] = React.useState(null)
   const [isPlaying, setIsPlaying] = React.useState(false)
-  const [nowPlaying, setNowPlaying] = React.useState("")
-  const [stationImage, setStationImage] = React.useState(null)
+  const [nowPlaying, setNowPlaying] = React.useState('')
+  // const [stationImage, setStationImage] = React.useState(null)
   const [isLoading, setIsLoading] = React.useState(false)
 
   React.useEffect(() => {
-    setupSounds();
-
-    document.body.addEventListener('click', event => {
-      if (event.target.tagName.toLowerCase() === 'a' && event.target.protocol != 'file:') {
-        event.preventDefault();
-        shell.openExternal(event.target.href);
+    document.body.addEventListener('click', (event) => {
+      if (
+        event.target.tagName.toLowerCase() === 'a' &&
+        event.target.protocol != 'file:'
+      ) {
+        event.preventDefault()
+        shell.openExternal(event.target.href)
       }
-    });
+    })
+
+    getStations()
   }, [])
 
+  async function getStations() {
+    const records = await airtableDb('table')
+      .select({
+        sort: [{ field: 'key', direction: 'asc' }],
+      })
+      .firstPage()
+    const stationsObj = {}
+
+    console.log(records)
+    records.forEach((station, ind) => {
+      stationsObj[station.fields.key] = {
+        name: station.fields.name,
+        homepage: station.fields.homepage,
+        stream: station.fields.stream,
+        nowPlaying: {
+          url: station.fields['nowPlaying.url'],
+          type: station.fields['nowPlaying.type'],
+          paths: station.fields['nowPlaying.paths']?.split(', '),
+          selector: station.fields['nowPlaying.selector'],
+          proxy: station.fields['nowPlaying.proxy'] ? true : false,
+        },
+      }
+    })
+
+    setStations(stationsObj)
+  }
+
   React.useEffect(() => {
-    updateNowPlaying();
+    setupSounds()
+  }, [stations])
+
+  React.useEffect(() => {
+    updateNowPlaying()
 
     ipcRenderer.on('media', (e, msg) => {
       console.log(msg)
-      if (msg === "playpause") {
-        toggleCurrentStation();
+      if (msg === 'playpause') {
+        toggleCurrentStation()
       }
 
-      if (msg === "next") {
-        const newStation = randomProperty(stations);
-        // console.log(newStation);
-        toggle(newStation);
+      if (msg === 'next') {
+        const newStation = randomProperty(stations)
+        toggle(newStation)
       }
-    });
+    })
 
     return () => {
-      ipcRenderer.removeAllListeners('media');
+      ipcRenderer.removeAllListeners('media')
     }
   }, [currentStation])
 
   useInterval(() => {
-    updateNowPlaying();
-  }, 5000);
+    updateNowPlaying()
+  }, 5000)
 
   useInterval(() => {
-    const height = document.querySelector('main').clientHeight + 12;
-    ipcRenderer.send('resize', height);
-  }, 100);
+    const height = document.querySelector('main').clientHeight + 12
+    ipcRenderer.send('resize', height)
+  }, 100)
 
   function setupSounds() {
     for (const key in stations) {
@@ -245,10 +103,10 @@ const App = () => {
           html5: true,
           autoplay: false,
           onplay: () => {
-            setIsPlaying(key);
-            setIsLoading(false);
+            setIsPlaying(key)
+            setIsLoading(false)
           },
-        });
+        })
       }
     }
   }
@@ -257,52 +115,53 @@ const App = () => {
     if (!currentStation || !currentStation.nowPlaying) return false
 
     function resolve(obj, path) {
-      path = path.split('.');
-      var current = obj;
+      path = path.split('.')
+      var current = obj
       while (path.length) {
-        if (typeof current !== 'object') return undefined;
-        current = current[path.shift()];
+        if (typeof current !== 'object') return undefined
+        current = current[path.shift()]
       }
-      return current;
+      return current
     }
 
     async function prepare(req) {
-      let results = "-";
-      
-      if (currentStation.nowPlaying.type === "json") {
-        results = await req.json();
+      let results = '-'
+
+      if (currentStation.nowPlaying.type === 'json') {
+        results = await req.json()
       }
-      if (currentStation.nowPlaying.type === "xml") {
-        const xml = await req.text();
-        results = xml2js(xml, {compact: true});
+      if (currentStation.nowPlaying.type === 'xml') {
+        const xml = await req.text()
+        results = xml2js(xml, { compact: true })
       }
-      if (currentStation.nowPlaying.type === "html") {
-        const htmlString = await req.text();
-        const domEle = stringToHTML(htmlString);
-        results = domEle.querySelector(currentStation.nowPlaying.selector).innerText.trim();
+      if (currentStation.nowPlaying.type === 'html') {
+        const htmlString = await req.text()
+        const domEle = stringToHTML(htmlString)
+        results = domEle
+          .querySelector(currentStation.nowPlaying.selector)
+          .innerText.trim()
       }
 
       return results
     }
 
-    let url = currentStation.nowPlaying.url;
+    let url = currentStation.nowPlaying.url
     if (currentStation.nowPlaying.proxy) {
       url = `${proxy}/${currentStation.nowPlaying.url}`
     }
-    const req = await fetch(url);
-    const results = await prepare(req);
+    const req = await fetch(url)
+    const results = await prepare(req)
 
     if (currentStation.nowPlaying.paths) {
       const parts = []
-      currentStation.nowPlaying.paths.forEach(path => {
-        parts.push(resolve(results, path));
-      });
+      currentStation.nowPlaying.paths.forEach((path) => {
+        parts.push(resolve(results, path))
+      })
 
-      setNowPlaying(`${parts.join(" - ")}`);
+      setNowPlaying(`${parts.join(' - ')}`)
     } else {
-      setNowPlaying(results);
+      setNowPlaying(results)
     }
-    
   }
 
   // async function updateStationImage() {
@@ -312,37 +171,41 @@ const App = () => {
   // }
 
   function toggle(station) {
-    stopAllStations();
-    
+    stopAllStations()
+
     if (!currentStation || currentStation.stream !== station.stream) {
-      setIsLoading(true);
-      setNowPlaying("-");
-      setCurrentStation(station);
-      station.sound.play();
+      setIsLoading(true)
+      setNowPlaying('-')
+      setCurrentStation(station)
+      station.sound.play()
     } else {
-      setCurrentStation(null);
+      setCurrentStation(null)
     }
   }
 
   function stopAllStations() {
     for (const key in stations) {
-      stations[key].sound.stop();
+      stations[key].sound.stop()
     }
-    setCurrentStation(null);
-    setIsPlaying(false);
-    setIsLoading(false);
+    setCurrentStation(null)
+    setIsPlaying(false)
+    setIsLoading(false)
   }
 
   function toggleCurrentStation() {
     if (!currentStation) return false
 
     if (currentStation.sound.playing()) {
-      currentStation.sound.stop();
-      setIsPlaying(false);
+      currentStation.sound.stop()
+      setIsPlaying(false)
     } else {
-      setIsLoading(true);
-      currentStation.sound.play();
+      setIsLoading(true)
+      currentStation.sound.play()
     }
+  }
+
+  function quitApp() {
+    ipcRenderer.send('app-quit')
   }
 
   return (
@@ -356,24 +219,42 @@ const App = () => {
 
       {currentStation && (
         <div className="now-playing">
-          <h2><a href={currentStation.homepage} target="_blank">{currentStation?.name}</a></h2>
+          <h2>
+            <a href={currentStation.homepage} target="_blank">
+              {currentStation?.name}
+            </a>
+          </h2>
           <h3>{nowPlaying}</h3>
         </div>
       )}
 
       <div className="stations">
         {Object.keys(stations).map((s, i) => {
-          return <button key={i} onClick={() => toggle(stations[s])} className={isPlaying === s ? `active` : ``}>{s.toUpperCase()}</button>
+          return (
+            <button
+              key={i}
+              onClick={() => toggle(stations[s])}
+              className={isPlaying === s ? `active` : ``}
+            >
+              {s.toUpperCase()}
+            </button>
+          )
         })}
       </div>
 
       {currentStation && (
         <div className="controls">
-          <button onClick={() => toggleCurrentStation()}>{isPlaying ? "Stop" : "Play"}</button>
+          <button onClick={() => toggleCurrentStation()}>
+            {isPlaying ? 'Stop' : 'Play'}
+          </button>
+
+          <button className="quit-app" onClick={() => quitApp()}>
+            Quit
+          </button>
         </div>
       )}
     </main>
   )
-};
+}
 
-export default App;
+export default App
--- src/server/main.js
+++ src/server/main.js
@@ -1,8 +1,8 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-const { app, BrowserWindow, globalShortcut, ipcMain } = require('electron');
-const { is } = require('electron-util');
-const { menubar } = require('menubar');
-const path = require('path');
+const { app, BrowserWindow, globalShortcut, ipcMain } = require('electron')
+const { is } = require('electron-util')
+const { menubar } = require('menubar')
+const path = require('path')
 
 const createMainWindow = (mb) => {
   // const mainWindow = new BrowserWindow({
@@ -16,48 +16,63 @@ const createMainWindow = (mb) => {
   //     contextIsolation: false,
   //   }
   // });
-  const mainWindow = mb.window;
+  const mainWindow = mb.window
 
-  const playPauseShortcut = globalShortcut.register('mediaplaypause', function () {
-    console.log('mediaplaypause pressed');
-    mainWindow.webContents.send('media', 'playpause');
-  });
+  const playPauseShortcut = globalShortcut.register(
+    'mediaplaypause',
+    function () {
+      console.log('mediaplaypause pressed')
+      mainWindow.webContents.send('media', 'playpause')
+    },
+  )
   if (!playPauseShortcut) {
-    console.log('mediaplaypause registration failed');
+    console.log('mediaplaypause registration failed')
   } else {
-    console.log('mediaplaypause registration bound!');
+    console.log('mediaplaypause registration bound!')
   }
 
-  const nextTrackShortcut = globalShortcut.register('medianexttrack', function () {
-    console.log('medianexttrack pressed');
-    mainWindow.webContents.send('media', 'next');
-  });
+  const nextTrackShortcut = globalShortcut.register(
+    'medianexttrack',
+    function () {
+      console.log('medianexttrack pressed')
+      mainWindow.webContents.send('media', 'next')
+    },
+  )
   if (!nextTrackShortcut) {
-    console.error('medianexttrack registration failed');
+    console.error('medianexttrack registration failed')
   }
 
   ipcMain.on('resize', (e, data) => {
     // console.log(e, data);
-    mb.window.setSize(400, data);
-  });
+    mb.window?.setSize(400, data)
+  })
+
+  ipcMain.on('app-quit', () => {
+    app.quit()
+  })
 
   mb.on('after-create-window', () => {
     if (is.development) {
-      mb.window.openDevTools({ mode: 'detach' });
+      mb.window.openDevTools({ mode: 'detach' })
       // mainWindow.loadURL('http://localhost:3000');
     } else {
       // mainWindow.loadURL(`file://${path.join(__dirname, '../../build/index.html')}`);
     }
   })
-};
+}
 
 app.on('ready', () => {
   let index = `file://${path.join(__dirname, '../../build/index.html')}`
   if (is.development) {
     index = 'http://localhost:3000'
   }
+  let icon = `file://${path.join(__dirname, '../../build/IconTemplate@2x.png')}`
+  if (is.development) {
+    icon = './IconTemplate.png'
+  }
+
   const mb = menubar({
-    icon: `./IconTemplate.png`,
+    icon: icon,
     index: index,
     browserWindow: {
       resizable: false,
@@ -71,9 +86,9 @@ app.on('ready', () => {
         contextIsolation: false,
       },
     },
-  });
+  })
 
   mb.on('ready', () => {
-    createMainWindow(mb);
-  });
-});
+    createMainWindow(mb)
+  })
+})
